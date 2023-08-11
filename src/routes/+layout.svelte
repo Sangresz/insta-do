4d9ac92fd1710e6db519d19adf1d2d6bdfc914d4
Corrado Cionini <corrado.cionini@protonmail.com>
2023-08-11 23:39:01 +0200

    feat: auth in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script>
+    import { supabase } from '$lib/supabaseClient';
+    import { user } from '$lib/stores';
+    import { onMount } from "svelte";
     import "../app.css";
+
+    onMount(() => {
+        supabase.auth.getSession().then(({ data }) => {
+            if(data.session?.user) $user = data.session.user
+        })
+    })
 </script>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,9 @@
 <script lang="ts">
-    import { supabase } from '$lib/supabaseClient';
-    import { user } from '$lib/stores'
-    import { onMount } from 'svelte';
-
-    onMount(() => {
-        console.log($user)
-        supabase.auth.getUser().then(({ data }) => {
-            if(data.user) $user = data.user
-        })
-        console.log($user)
-    })
+    import { user } from "$lib/stores";
 </script>
 
 
 {#if $user != undefined}
-    <p>{$user.id}</p>
     <h1 class="text-3xl">Welcome to SvelteKit</h1>
     <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
     <p>Autenticato</p>
